81c4c050fa7cc7a5355a8a847c7fcd2d802cba74
Mayara Karen Marques Pereira <karen.marques@compasso.com.br>
2024-11-05 20:57:09 -0300

    fix(api): Add custom object loading for TensorFlow model and ensure temporary upload folder creation in API

--- api.py
+++ api.py
@@ -3,12 +3,14 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
+from modelo import thresholding, crop_and_roi  # Importe as funções personalizadas
 
 app = Flask(__name__)
 
-# Carregar o modelo treinado
+# Carregar o modelo treinado com as funções personalizadas
 try:
-    model = load_model('bovino_classification_model.h5')
+    model = load_model('bovino_classification_model.h5', custom_objects={'crop_and_roi': crop_and_roi, 'thresholding': thresholding})
+    print("Modelo carregado com sucesso.")
 except Exception as e:
     print(f"Erro ao carregar o modelo: {e}")
     exit(1)
@@ -18,10 +20,10 @@ class_labels = ['Dermatite Nodular', 'Berne', 'Saudável']
 
 def preprocess_image(img_path):
     """Carrega e pré-processa a imagem antes de passar para o modelo."""
-    img = image.load_img(img_path, target_size=(64, 64))  # Ajustar o tamanho conforme o que foi usado no treinamento
+    img = image.load_img(img_path, target_size=(64, 64))  # Ajuste conforme o tamanho usado no treinamento
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)  # Adicionar uma dimensão extra
-    img_array /= 255.0  # Normalizar a imagem para os valores entre 0 e 1
+    img_array /= 255.0  # Normalizar para valores entre 0 e 1
     return img_array
 
 @app.route('/predict', methods=['POST'])
@@ -44,7 +46,7 @@ def predict():
     # Fazer a predição
     predictions = model.predict(img_array)
     predicted_class_idx = np.argmax(predictions)
-    confidence = np.max(predictions) * 100  # A confiança da previsão
+    confidence = np.max(predictions) * 100  # Confiança da previsão
 
     # Pegar o nome da classe prevista
     predicted_class = class_labels[predicted_class_idx]
